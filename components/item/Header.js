5639c89fcdc649226af4cc9b95eac90fd3666464
AhmadHaider-kw <ahmad.haider.kw@gmail.com>
2022-05-22 17:19:55 +0300

    readable css to work, but a lot of work

--- components/item/Header.js
+++ components/item/Header.js
@@ -5,12 +5,14 @@ export default function Header() {
 	return (
 		<div className={classes.card}>
 			<div className={classes.header}>
-				<Image src='/images/test.jpg' alt='' width={396} height={300}></Image>
+				<div className={classes.Image}>
+					<Image src='/images/test.jpg' alt='' width={420} height={400}></Image>
+				</div>
 			</div>
 
 			<div className={classes.card_body}>
 				<div className={classes.card_info}>
-					<h1 className={classes.card_title}>milk chocolate cookie</h1>
+					<h1 className={classes.card_title}>milk chocolate chips cookie</h1>
 					<p className={classes.card_description}>12 piece of sexy cookies</p>
 					<button className={classes.card_price}>7.000 KWD</button>
 				</div>
@@ -20,15 +22,15 @@ export default function Header() {
 						<h5>extra piece</h5>
 						<button>extra dose</button>
 					</div>
-					<div className={classes.card_details_group}>
+					<div className={classes.card_details_group2}>
 						<h5>extra sauce</h5>
 						<button>kinder sauce</button>
 						<button>pussy sauce</button>
 						<button>vigina sauce</button>
 					</div>
-					<div className={classes.card_details_group}>
+					<div className={classes.message}>
 						<h5>leave your message</h5>
-						<textarea>textarea</textarea>
+						<textarea className={classes.textarea}>textarea</textarea>
 					</div>
 				</div>
 			</div>
@@ -46,7 +48,17 @@ export default function Header() {
 						height={12}
 					/>
 				</div>
-				<div>footer</div>
+				<div className={classes.addToCart}>
+					<h2>Add to cart</h2>
+					<div>
+						<Image
+							src='/images/svg/shopping-cart-10941.svg'
+							width={24}
+							height={24}
+							alt=''
+						/>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
