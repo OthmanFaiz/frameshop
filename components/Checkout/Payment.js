9d011f0d3116db596e1cd77b7f91bc3e385e5105
AhmadHaider-kw <ahmad.haider.kw@gmail.com>
2022-05-24 16:03:48 +0300

    basic form submit

--- components/Checkout/Payment.js
+++ components/Checkout/Payment.js
@@ -5,22 +5,23 @@ import Image from 'next/image';
 import { useCart } from '../../lib/CartContext';
 
 export default function Payment() {
-
 	const { cart } = useCart();
 	const [totalPrice, setTotalPrice] = useState(0);
 	useEffect(() => {
-		if(Object.entries(cart).length > 0) {
-			setTotalPrice(Object.keys(cart).reduce((acc, cur) =>{
-				if (cart[cur].quantity > 1) {
-					return acc + (cart[cur].quantity * cart[cur].price)
-				}
-				return acc + cart[cur].price
-				}, 0))
+		if (Object.entries(cart).length > 0) {
+			setTotalPrice(
+				Object.keys(cart).reduce((acc, cur) => {
+					if (cart[cur].quantity > 1) {
+						return acc + cart[cur].quantity * cart[cur].price;
+					}
+					return acc + cart[cur].price;
+				}, 0)
+			);
 		}
-	},[cart])
+	}, [cart]);
 	return (
 		<>
-			<div className={classes.title}>
+			<section className={classes.title}>
 				<h1 className={classes.info}>Payment</h1>
 
 				<div className={`${classes.flex} ${classes.flex__col}`}>
@@ -32,12 +33,109 @@ export default function Payment() {
 						<h3>Delivery</h3>
 						<h3>1.000 KWD</h3>
 					</div>
-					<div className={classes.section}>
+					<div className={classes.price}>
 						<h1>Total Amount</h1>
 						<h1>{(totalPrice + 1).toFixed(2)} KWD</h1>
 					</div>
 				</div>
-			</div>
+
+				<section className={classes.Section_specialRequest}>
+					<h1 className={classes.info}>Speical request</h1>
+					<textarea className={classes.specialRequest_textarea}>e</textarea>
+				</section>
+
+				<section className={classes.Section_form}>
+					<h1 className={classes.info}>Your Address</h1>
+
+					<form className={classes.form}>
+						<div className={classes.control}>
+							<label htmlFor='AddressType'></label>
+							<input
+								type='text'
+								required
+								id='AddressType'
+								placeholder='Address type'
+							></input>
+						</div>
+						<div className={classes.control}>
+							<label htmlFor='AddressName'></label>
+							<input
+								type='text'
+								required
+								id='AddressName'
+								placeholder='Address Name'
+							></input>
+						</div>
+						<div className={classes.control}>
+							<label htmlFor='PhoneNumber'></label>
+							<input
+								type='text'
+								required
+								id='PhoneNumber'
+								placeholder='Phone Number'
+							></input>
+						</div>
+						<div className={classes.control}>
+							<label htmlFor='Town'></label>
+							<input type='text' required id='Town' placeholder='Town'></input>
+						</div>
+						<div className={classes.control}>
+							<label htmlFor='Block'></label>
+							<input
+								type='text'
+								required
+								id='Block'
+								placeholder='Block'
+							></input>
+						</div>{' '}
+						<div className={classes.control}>
+							<label htmlFor='Street'></label>
+							<input
+								type='text'
+								required
+								id='Street'
+								placeholder='Street'
+							></input>
+						</div>{' '}
+						<div className={classes.control}>
+							<label htmlFor='Lane'></label>
+							<input
+								type='text'
+								required
+								id='Lane'
+								placeholder='Lane(optional)'
+							></input>
+						</div>{' '}
+						<div className={classes.control}>
+							<label htmlFor='House'></label>
+							<input
+								type='text'
+								required
+								id='House'
+								placeholder='House/Building'
+							></input>
+						</div>{' '}
+						<div className={classes.control}>
+							<label htmlFor='Floor'></label>
+							<input
+								type='text'
+								required
+								id='Floor'
+								placeholder='Floor'
+							></input>
+						</div>{' '}
+						<div className={classes.control}>
+							<label htmlFor='Apartment'></label>
+							<input
+								type='text'
+								required
+								id='Apartment'
+								placeholder='Apartment'
+							></input>
+						</div>
+					</form>
+				</section>
+			</section>
 		</>
 	);
 }
--- todo.md
+++ todo.md
@@ -5,28 +5,31 @@
 - [ ] adreline front page layout
 
 - [ ] header ( needs more tweaks )
-	- [ ] few margin issue for the whole page
-	- [ ] the navigation margin maybe shift to left little bit
+
+  - [ ] few margin issue for the whole page
+  - [ ] the navigation margin maybe shift to left little bit
 
 - [ ] body (little style tweek)
-	- [x] fixed the fonts size in global css 
-	- [x] card layout been made
-	- [x] item counter 
-	- [x] item counter functionallty
-	- [x] separte item counter functionallty
-		bug: when the item is deleted, the counter will not update 
-	- [x] connect item counter to footer
+
+  - [x] fixed the fonts size in global css
+  - [x] card layout been made
+  - [x] item counter
+  - [x] item counter functionallty
+  - [x] separte item counter functionallty
+        bug: when the item is deleted, the counter will not update
+  - [x] connect item counter to footer
 
 - [x] footer
-	- [x] main footer layout
-	- [ ] footer style tweek 
-	- [x] functionalty for the footer
+
+  - [x] main footer layout
+  - [ ] footer style tweek
+  - [x] functionalty for the footer
 
 - [ ] adreline popout layout
 
 - [ ] adreline checkout layout
-
-  - [ ] main layout
+  - [ ] main layout jxs
+  - [ ] beta css style
   - [ ] react form for the address
 
 ## Back end todo list
@@ -36,8 +39,10 @@
 - [ ] Adrinalin frontpage (data from the DB)
 
 ### 23 - 5 - 2022 " Othman "
+
 - [x] clean the main page ( delete the plus & minus sign )
 - [x] add functionallty to the add to cart page
-	need's some tweaks ( updating the cart when the item is added )
+      need's some tweaks ( updating the cart when the item is added )
 - [x] add functionallty to the checkout page
-	- [x] we need to use contextAPI to accesses the state from another rout
+  - [x] we need to use contextAPI to accesses the state from another rout
+- [] collect the data from react form and add them to database
