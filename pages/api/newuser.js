90c311f3813d8508bde4811256bba9997fb82626
Othman Al-Omair <info@othmanfaiz.com>
2022-07-12 18:59:15 +0300

    tokens

--- pages/api/newuser.js
+++ pages/api/newuser.js
@@ -1,11 +1,13 @@
 import clientPromise from "../../lib/mongodb";
+import jwt from "jsonwebtoken";
 
 export default async function handler(req,res) {
     try {
         const client = await clientPromise;
         const db = client.db("adrenaline");
         const user = req.body;
-        const newuser = await db.collection("users").insertOne(user);
+        const newuser = await db.collection("users").insertOne({...user,token: jwt.sign({email: user.email}, process.env.JWT_SECRET, {expiresIn: "30d"})});
+        
         res.status(200).json({state: "success", newuser});
     } catch (err) {
         console.log(err);
