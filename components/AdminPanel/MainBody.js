e0c7bee25d4c187acf27253ad391478e13386347
AhmadHaider-kw <ahmad.haider.kw@gmail.com>
2022-07-29 20:39:38 +0300

    babe, keep working on mainBody.js , and remove the links and add the usestate for toggling between the page content , love u

--- components/AdminPanel/MainBody.js
+++ components/AdminPanel/MainBody.js
@@ -1,7 +1,90 @@
-import classes from './Main.module.css';
+import classes from './MainBody.module.css';
+import Image from 'next/image';
 
 export default function MainBody() {
-    return (
-            <h1>Hello World!</h1>
-    )
+	return (
+		<>
+			<section className={classes.Layout}>
+				<div className={classes.topLayout}>
+					<div className={classes.topLayout_box}>
+						<div className={classes.topLayout_box_img}>
+							<Image
+								src={'/images/svg/arrow-upright.svg'}
+								alt=''
+								width={25}
+								height={25}
+							/>
+						</div>
+						<div className={classes.topLayout_box_texts}>
+							<h4 className={classes.topLayout_box_price}>700.00 KWD</h4>
+							<p className={classes.topLayout_box_desc}>Today Revenue</p>
+						</div>
+					</div>
+					<div className={classes.topLayout_box}>
+						<div className={classes.topLayout_box_img2}>
+							<Image
+								src={'/images/svg/printer.svg'}
+								alt=''
+								width={25}
+								height={25}
+							/>
+						</div>
+						<div className={classes.topLayout_box_texts}>
+							<h4 className={classes.topLayout_box_price}>700.00kwd</h4>
+							<p className={classes.topLayout_box_desc}>Today Revenue</p>
+						</div>
+					</div>
+					<div className={classes.topLayout_box}>
+						<div className={classes.topLayout_box_img3}>
+							<Image
+								src={'/images/svg/customers.svg'}
+								alt=''
+								width={25}
+								height={25}
+							/>
+						</div>
+						<div className={classes.topLayout_box_texts}>
+							<h4 className={classes.topLayout_box_price}>700.00kwd</h4>
+							<p className={classes.topLayout_box_desc}>Today Revenue</p>
+						</div>
+					</div>
+					<div className={classes.topLayout_box}>
+						<div className={classes.topLayout_box_img4}>
+							<Image
+								src={'/images/svg/zigzagi-arrow.svg'}
+								alt=''
+								width={25}
+								height={25}
+							/>
+						</div>
+						<div className={classes.topLayout_box_texts}>
+							<h4 className={classes.topLayout_box_price}>700.00kwd</h4>
+							<p className={classes.topLayout_box_desc}>Today Revenue</p>
+						</div>
+					</div>
+				</div>
+				{/* main layout section */}
+				<div className={classes.MainLayout}>
+					<div className={classes.MainLayout_graph}>
+						<h1>hello shityy world</h1>
+					</div>
+					<div className={classes.customerInfo}>
+						<h1>hello motherfuckers</h1>
+					</div>
+				</div>
+
+				<div className={classes.LowerLayout}>
+					<div className={classes.LowerLayout_card}>
+						<h1>hello creepy</h1>
+					</div>
+					<div className={classes.LowerLayout_card}>
+						<h1>screw u</h1>
+					</div>
+					<div className={classes.LowerLayout_card}>
+						<h1>zusammengehorigkeitengefuhle</h1>
+					</div>
+				</div>
+			</section>
+		</>
+	);
 }
--- components/AdminPanel/NavBar.js
+++ components/AdminPanel/NavBar.js
@@ -2,39 +2,33 @@ import classes from './Main.module.css';
 import Image from 'next/image';
 
 export default function NavBar() {
-    return (
-        <section className={classes.header}>
-        <div className={classes.header_logo}>
-            <Image
-                src={'/images/svg/logo1.svg'}
-                alt=''
-                width={165}
-                height={54}
-                className={classes.header_logo_img}
-            />
-        </div>
-        <div className={classes.header_searchBar}>
-            <input placeholder='SEARCH FOR SOMETHING HERE... '></input>
-        </div>
+	return (
+		<section className={classes.header}>
+			<div className={classes.header_logo}>
+				<Image src={'/images/svg/logo1.svg'} alt='' width={165} height={54} />
+			</div>
+			<div className={classes.header_searchBar}>
+				<input placeholder='SEARCH FOR SOMETHING HERE... '></input>
+			</div>
 
-        <div className={classes.header_user}>
-            <div className={classes.header_user_details}>
-                <Image src={'/images/svg/bell.svg'} alt='' width={24} height={24} />
-                <Image
-                    src={'/images/svg/unsplash.svg'}
-                    alt=''
-                    width={24}
-                    height={24}
-                />
-                <h2 className={classes.header_user_details_name}>Othman Alomair</h2>
-                <Image
-                    src={'/images/svg/arrow-bottom.svg'}
-                    alt=''
-                    width={24}
-                    height={24}
-                />
-            </div>
-        </div>
-        </section>
-    )
+			<div className={classes.header_user}>
+				<div className={classes.header_user_details}>
+					<Image src={'/images/svg/bell.svg'} alt='' width={24} height={24} />
+					<Image
+						src={'/images/svg/unsplash.svg'}
+						alt=''
+						width={24}
+						height={24}
+					/>
+					<h2 className={classes.header_user_details_name}>Othman Alomair</h2>
+					<Image
+						src={'/images/svg/arrow-bottom.svg'}
+						alt=''
+						width={24}
+						height={24}
+					/>
+				</div>
+			</div>
+		</section>
+	);
 }
